6a1808bfc631a0ef7f722ad43ab353e006adcc67
Julieth Quinchia <j.quinchia@uniandes.edu.co>
2024-09-14 16:17:11 -0500

    Erros in connections

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -171,7 +171,7 @@ MQTT_PASSWORD_PUB = "admin2"
 
 # Tópico a suscribir. "+/+/+/+/out" se suscribe únicamente a los
 # tópicos con forma <país>/<estado>/<ciudad>/<usuario>/out.
-TOPIC = "colombia/antioquia/rionegro/admin/out"
+TOPIC = "colombia/antioquia/rionegro/admin2/out"
 
 # Opción para habilitar la transmisión de mensajes segura
 MQTT_USE_TLS = False
