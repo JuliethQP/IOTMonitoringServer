3e56098605e0fbbef7c95d1ff1dc7296fac23318
Julieth Quinchia <j.quinchia@uniandes.edu.co>
2024-09-14 15:07:03 -0500

    Change users in mqtt

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -162,12 +162,12 @@ MQTT_HOST = "44.204.98.226"
 MQTT_PORT = 8082
 
 # Credenciales para el usuario suscriptor del bróker MQTT
-MQTT_USER = "admin"
-MQTT_PASSWORD = "admin"
+MQTT_USER = "juliethquinchia"
+MQTT_PASSWORD = "orion123"
 
 # Credenciales para el usuario publicador del bróker MQTT
-MQTT_USER_PUB = "admin2"
-MQTT_PASSWORD_PUB = "admin2"
+MQTT_USER_PUB = "juliethquinchia"
+MQTT_PASSWORD_PUB = "orion123"
 
 # Tópico a suscribir. "+/+/+/+/out" se suscribe únicamente a los
 # tópicos con forma <país>/<estado>/<ciudad>/<usuario>/out.
