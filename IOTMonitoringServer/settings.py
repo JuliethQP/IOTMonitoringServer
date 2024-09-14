8529174a7b7d18d01c5e9179b194be101e05c508
Julieth Quinchia <j.quinchia@uniandes.edu.co>
2024-09-14 08:37:35 -0500

    Update ip db

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "44.222.146.133",  # Dirección IP de la base de datos
+        "HOST": "3.238.55.56",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
