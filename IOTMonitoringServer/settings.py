2d989ffa572b9209d6b2116853075ce091fe1705
Julieth Quinchia <j.quinchia@uniandes.edu.co>
2024-09-14 19:10:04 -0500

    Update ip broker

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.216.79.109"]
+ALLOWED_HOSTS = ["localhost", "44.200.226.142"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.239.70.54",  # Dirección IP de la base de datos
+        "HOST": "34.236.237.206",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
